09d66a649bc56d60084b0e6b5e94f1e68ddede99
Zokir Tiliaev <zakirenish@gmail.com>
2018-10-21 18:40:50 -0400

    Made LUME modifications

--- snapshot_generator.py
+++ snapshot_generator.py
@@ -1,15 +1,19 @@
 import pandas as pd
 
-DECIMAL_PRECISION=3
 EOS_SNAPSHOT_FILE='eos_snapshot.csv'
-TOKEN_MULTIPLIER=5.1 # How many airdropped tokens to send per EOS
-OUTPUT_FILE='iq_snapshot.csv'
-USE_BLACKLIST=True
+TOKEN_MULTIPLIER=0.05 # How many airdropped tokens to send per EOS
+OUTPUT_FILE='lume_snapshot.csv'
+LUME_LIMIT=2000
+
+ACCT_NAME_INDEX=1
+EOS_INDEX=2
+LUME_INDEX=3
 
 df = pd.read_csv(EOS_SNAPSHOT_FILE, header=None)
-df[4] = df[3] * TOKEN_MULTIPLIER
+df = df.drop(0, axis=1)
+df[LUME_INDEX] = df[EOS_INDEX] * TOKEN_MULTIPLIER
 
-# Blacklist 
+# Blacklist
 with open('blacklist.txt', 'r') as f:
     blacklist = f.read()
 with open('whitelist.txt', 'r') as f:
@@ -18,10 +22,13 @@ blacklist = [line.strip() for line in blacklist.split('\n') if line is not '' an
 whitelist = [line.strip() for line in whitelist.split('\n') if line is not '' and line[0] is not '#']
 intersection = [pubkey for pubkey in whitelist if pubkey in blacklist]
 assert len(intersection) == 0, "No keys in the whitelist should be in the blacklist: {0}".format(intersection)
-df = df[~df[2].isin(blacklist)]
+df = df[~df[ACCT_NAME_INDEX].isin(blacklist)]
+
+# apply limit
+df[LUME_INDEX] = df[LUME_INDEX].map(lambda x: min(float(x), LUME_LIMIT))
 
 # Decimal precision
-df[4] = df[4].map(lambda x: '{0:.3f}'.format(x))
-df[3] = df[3].map(lambda x: '{0:.4f}'.format(x))
+df[LUME_INDEX] = df[LUME_INDEX].map(lambda x: '{0:.3f}'.format(x))
+df[EOS_INDEX] = df[EOS_INDEX].map(lambda x: '{0:.4f}'.format(x))
 
 df.to_csv(OUTPUT_FILE, index=False, header=False)
--- validate_airdrop.sh
+++ validate_airdrop.sh
@@ -1,16 +1,17 @@
-SYMBOL="DRYC"
-ISSUER="iqairdropper"
-VALIDATIONS=1000
+SYMBOL="LUME"
+ISSUER="lumeostokens"
+VALIDATIONS=2
+SNAPSHOT_FILE=lume_snapshot.csv
 
-NUM_LINES=$(wc -l iq_snapshot.csv | awk '{print $1}')
+NUM_LINES=$(wc -l "${SNAPSHOT_FILE}" | awk '{print $1}')
 FAIL_FLAG=false
-for i in `seq 1 $VALIDATIONS`; do 
+for i in `seq 1 $VALIDATIONS`; do
     RAND=$(openssl rand 4 | od -DAn)
     RAND=$((RAND % $NUM_LINES))
-    LINE=$(sed "${RAND}q;d" iq_snapshot.csv)
-    ACCOUNT=$(echo $LINE | tr "," "\n" | head -2 | tail -1)
-    AMOUNT=$(echo $LINE | tr "," "\n" | tail -1)
-    CURRENT_BALANCE=$(cleos get table $ISSUER $ACCOUNT accounts | grep $SYMBOL | grep -Eo '[0-9]+\.[0-9]+')
+    LINE=$(sed "${RAND}q;d" "${SNAPSHOT_FILE}")
+    ACCOUNT=$(echo $line | tr "," "\n" | head -1 | tail -1)
+    AMOUNT=$(echo $line | tr "," "\n" | tail -1)
+    CURRENT_BALANCE=$(cl get table $ISSUER $ACCOUNT accounts | grep $SYMBOL | grep -Eo '[0-9]+\.[0-9]+')
     #echo "$ACCOUNT $AMOUNT $CURRENT_BALANCE"
     if [ "$AMOUNT" != "$CURRENT_BALANCE" ]; then
         echo "$ACCOUNT failed. Should have $AMOUNT $SYMBOL"
